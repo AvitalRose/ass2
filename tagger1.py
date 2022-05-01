ec8016e3813e86c278aaaf6c6a2fcd7cdca8e850
avital <avitalrose1997@gmail.com>
2022-05-02 02:25:53 +0300

    after making it work for ner as well

--- tagger1.py
+++ tagger1.py
@@ -1,3 +1,4 @@
+# Avital Rose 318413408
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,13 +26,13 @@ class NGramLanguageModeler(nn.Module):
         return log_probs
 
 
-def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_ix, label_to_index):
+def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_index, label_to_index):
     total_loss = 0
     train_correct = 0
-    for context, target in five_grams:
+    for context, target in ngrams:
         # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
         # into integer indices and wrap them in tensors)
-        context_idxs = torch.tensor([word_to_ix.get(w, 0) for w in context], dtype=torch.long)
+        context_idxs = torch.tensor([word_to_index.get(w, 0) for w in context], dtype=torch.long)
 
         # Step 2. Recall that torch *accumulates* gradients. Before passing in a
         # new instance, you need to zero out the gradients from the old
@@ -56,7 +57,8 @@ def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_ix, label_t
             train_correct += 1
     return total_loss, 100 * train_correct / len(ngrams)
 
-def validate(_model, _loss_function, valid_ngrams, label_to_index):
+
+def validate(_model, _loss_function, valid_ngrams, word_to_index, label_to_index):
     """
 
     :return:
@@ -64,7 +66,7 @@ def validate(_model, _loss_function, valid_ngrams, label_to_index):
     valid_loss = 0
     valid_correct = 0
     for context, target in valid_ngrams:
-        context_idxs = torch.tensor([word_to_ix.get(w, 0) for w in context], dtype=torch.long)
+        context_idxs = torch.tensor([word_to_index.get(w, 0) for w in context], dtype=torch.long)
         log_probs = _model(context_idxs)
         loss = _loss_function(log_probs, torch.tensor([label_to_index[target]], dtype=torch.long))
         valid_loss += loss.item()
@@ -91,7 +93,7 @@ def transform_data_to_ngrams(file_name):
         lines = f.readlines()
 
     # make up vocabulary as well as possible tags
-    lines_split = [(i.split(' ')[0], i.split(' ')[1][:-1]) for i in lines if len(i) > 1]  # length of empty lines is 0
+    lines_split = [(i.split()[0], i.split()[1].split("\n")[0]) for i in lines if len(i) > 1]  # length of empty lines is 0
     vocab_with_special_characters = set([j[0] for j in lines_split])  # ends up being 7896 words or 43127
     vocab_with_special_characters = [minus_two_word, minus_one_word] + list(vocab_with_special_characters) + \
                                     [plus_one_word, plus_two_word]
@@ -103,8 +105,8 @@ def transform_data_to_ngrams(file_name):
     j = 0
     for i in lines:
         if len(i) > 1:
-            sentences[j].append(i.split(' ')[0])
-            labels.append(i.split(' ')[1][:-1])
+            sentences[j].append(i.split()[0])
+            labels.append(i.split()[1].split("\n")[0])
         else:
             j += 1
             sentences.append([])
@@ -133,10 +135,10 @@ def transform_data_to_ngrams(file_name):
     return zipped_features_and_labels, vocab_with_special_characters, pos_vocab
 
 
-if __name__ == "__main__":
+def tagger(task, train_file, valid_file, test_file):
     # prepare data
-    five_grams, vocab, pos_tags = transform_data_to_ngrams("pos/train")
-    five_grams_valid, vocab_valid, pos_tags_valid = transform_data_to_ngrams("pos/dev")
+    five_grams, vocab, pos_tags = transform_data_to_ngrams(train_file)
+    five_grams_valid, vocab_valid, pos_tags_valid = transform_data_to_ngrams(valid_file)
 
     # make word to index dictionary
     word_to_ix = {word: i for i, word in enumerate(vocab, start=1)}
@@ -148,7 +150,7 @@ if __name__ == "__main__":
     train_loader = DataLoader(five_grams, batch_size=128, shuffle=True)
 
     print("train loader is: ", train_loader)
-    five_grams = five_grams[0:5000]
+    five_grams = five_grams[0:100]
     five_grams_valid = five_grams_valid[0:100]
     print("length of five grams is: ", len(five_grams), five_grams[0])
     print("length of five grams valid is :", len(five_grams_valid), five_grams_valid[0])
@@ -156,20 +158,18 @@ if __name__ == "__main__":
     losses = []
     accuracies = []
     losses_valid = []
-    accuracies_valid =[]
+    accuracies_valid = []
     lr = 0.001
     loss_function = nn.NLLLoss()
     model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, len(label_to_ix.keys()))
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
-    for epoch in range(5):
+    for epoch in range(3):
         print("epoch is: {}".format(epoch))
-        total_loss = 0
-        train_correct = 0
         loss_t, accuracy_t = train(model, optimizer, loss_function, vocab, five_grams, word_to_ix, label_to_ix)
         losses.append(loss_t)
         accuracies.append(accuracy_t)
-        loss_v, accuracy_v = validate(model, loss_function, five_grams_valid, label_to_ix)
+        loss_v, accuracy_v = validate(model, loss_function, five_grams_valid, word_to_ix, label_to_ix)
         losses_valid.append(loss_v)
         accuracies_valid.append(accuracy_v)
 
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     plt.plot(losses_valid, label="validation")
     plt.xlabel("epochs")
     plt.ylabel("loss")
-    plt.title("Loss according to epochs, learning rate = {}".format(lr))
+    plt.title("{}\nLoss according to epochs, learning rate = {}".format(task, lr))
     plt.legend()
     plt.show()
 
@@ -187,9 +187,14 @@ if __name__ == "__main__":
     plt.plot(accuracies_valid, label="validation")
     plt.xlabel("epochs")
     plt.ylabel("accuracy")
-    plt.title("Accuracy according to epochs, learning rate = {}".format(lr))
+    plt.title("{}\nAccuracy according to epochs, learning rate = {}".format(task,lr))
     plt.legend()
     plt.show()
+
+
+if __name__ == "__main__":
+    tagger("POS tagger", train_file="pos/train", valid_file="pos/dev", test_file="pos/test")
+    tagger("NER tagger", train_file="ner/train", valid_file="ner/dev", test_file="ner/test")
 
 """
 To do:
@@ -199,4 +204,5 @@ To do:
 * what to do with words not in train? right now it is getting the first row always. is dict.get(k) slower than dict[k]? 
 * add test and write to file
 * answer questions for part 1- first thing tomorrow morning
+* add sentence spaces in correct place
 """
