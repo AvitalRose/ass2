e888920d1e5a959cf1e76e6e5fa600f898f78f7c
avital <avitalrose1997@gmail.com>
2022-05-03 13:41:13 +0300

    before cleanup

--- tagger1.py
+++ tagger1.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 
 CONTEXT_SIZE = 5
 EMBEDDING_DIM = 50
-BATCH_SIZE = 128
+BATCH_SIZE = 512
+
 
 class NGramLanguageModeler(nn.Module):
 
@@ -19,11 +20,11 @@ class NGramLanguageModeler(nn.Module):
         self.linear2 = nn.Linear(128, tags_size)
 
     def forward(self, inputs):
-        # m = nn.Dropout(p=0.3)
+        m = nn.Dropout(p=0.3)
         embeds = self.embeddings(inputs.t())
         embeds = embeds.reshape(embeds.shape[0], CONTEXT_SIZE * EMBEDDING_DIM)
         out = F.relu(self.linear1(embeds))
-        # out = m(out)
+        out = m(out)
         out = self.linear2(out)
         log_probs = F.log_softmax(out, dim=1)
         return log_probs
@@ -33,7 +34,6 @@ def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_index, labe
     total_loss = 0
     train_correct = 0
     for idx, (context, target) in enumerate(ngrams):
-        # print("idx is {}".format(idx))
         # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
         # into integer indices and wrap them in tensors)
         # context_idxs = torch.tensor([word_to_index.get(w, 0) for w in context], dtype=torch.long)
@@ -42,9 +42,6 @@ def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_index, labe
             context_idxs.append(tuple((word_to_index.get(w, 0) for w in tup)))
         context_idxs = torch.tensor(context_idxs, dtype=torch.long)
 
-        # print(f"{context=}")
-        # print("context idxs length: ", context_idxs)
-        # print("context idxs shape: ", context_idxs.shape)
         # Step 2. Recall that torch *accumulates* gradients. Before passing in a
         # new instance, you need to zero out the gradients from the old
         # instance
@@ -62,17 +59,12 @@ def train(_model, _optimizer, _loss_function, vocab, ngrams, word_to_index, labe
         # Step 5. Do the backward pass and update the gradient
         loss.backward()
         _optimizer.step()
+
         # Get the Python number from a 1-element Tensor by calling tensor.item()
         total_loss += _loss_function(log_probs, torch.tensor(t, dtype=torch.long)).item()
         pred = log_probs.max(1, keepdim=True)[1]  # get the index of the max log-probability
         train_correct += pred.eq(torch.tensor(t, dtype=torch.long).view_as(pred)).cpu().sum().item()
-
-        #from beore
-        # total_loss += loss.item()
-        # pred = log_probs.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        # if pred.item() == label_to_index[target]:
-        #     train_correct += 1
-    return total_loss, (100 * train_correct) / input_len
+    return total_loss/input_len, (100 * train_correct) / input_len
 
 
 def validate(_model, _loss_function, valid_ngrams, word_to_index, label_to_index, input_len):
@@ -82,18 +74,12 @@ def validate(_model, _loss_function, valid_ngrams, word_to_index, label_to_index
     """
     valid_loss = 0
     valid_correct = 0
+    results = []
+    _model.eval()
     for context, target in valid_ngrams:
-        # log_probs = _model(context_idxs)
-        # loss = _loss_function(log_probs, torch.tensor([label_to_index[target]], dtype=torch.long))
-        # valid_loss += loss.item()
-        # pred = log_probs.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        # if pred.item() == label_to_index[target]:
-        #     valid_correct += 1
-
         context_idxs = []
         for tup in context:
             context_idxs.append(tuple((word_to_index.get(w, 0) for w in tup)))
-        # print("context id shape: ", context_idxs)
         context_idxs = torch.tensor(context_idxs, dtype=torch.long)
         log_probs = _model(context_idxs)
         t = tuple((label_to_index[t] for t in target))
@@ -102,11 +88,26 @@ def validate(_model, _loss_function, valid_ngrams, word_to_index, label_to_index
         valid_loss += _loss_function(log_probs, torch.tensor(t, dtype=torch.long)).item()
         pred = log_probs.max(1, keepdim=True)[1]  # get the index of the max log-probability
         valid_correct += pred.eq(torch.tensor(t, dtype=torch.long).view_as(pred)).cpu().sum().item()
+        pred = pred.tolist()
+        results.extend(pred)
+    return valid_loss / input_len, (100 * valid_correct) / input_len, results
 
-    return valid_loss, (100 * valid_correct) / input_len
 
+def predict(_model, _loss_function, valid_ngrams, word_to_index, label_to_index, input_len):
+    results = []
+    _model.eval() # switch layers which act different in train and test, like dropout etc
+    for context in valid_ngrams:
+        context_idxs = []
+        for tup in context:
+            context_idxs.append(tuple((word_to_index.get(w, 0) for w in tup)))
+        context_idxs = torch.tensor(context_idxs, dtype=torch.long)
+        log_probs = _model(context_idxs)
+        pred = log_probs.max(1, keepdim=True)[1]
+        results.extend(pred.tolist())
+    print("Results: ", results[0:20], len(results))
+    return results
 
-def transform_data_to_ngrams(file_name):
+def transform_data_to_ngrams(file_name, test=False):
     """
     this function transforms the data to five grams and target, as well as finds the possible vocabulary and tags
     :param file_name:
@@ -123,11 +124,20 @@ def transform_data_to_ngrams(file_name):
         lines = f.readlines()
 
     # make up vocabulary as well as possible tags
-    lines_split = [(i.split()[0], i.split()[1].split("\n")[0]) for i in lines if len(i) > 1]  # length of empty lines is 0
+    if not test:
+        lines_split = [(i.split()[0], i.split()[1].split("\n")[0]) for i in lines if len(i) > 1]
+    else:
+        lines_split = [(i.split()[0]) for i in lines if len(i) > 1]
     vocab_with_special_characters = set([j[0] for j in lines_split])  # ends up being 7896 words or 43127
     vocab_with_special_characters = [minus_two_word, minus_one_word] + list(vocab_with_special_characters) + \
                                     [plus_one_word, plus_two_word]
-    pos_vocab = set([j[1] for j in lines_split])
+    if not test:
+        pos_vocab = set([j[1] for j in lines_split])
+
+    # find empty spaces if test
+    if test:
+        empty_spaces = [index for index in range(len(lines)) if len(lines[index]) <= 1]
+
 
     # create sentences- by removing empty spaces
     sentences = [[]]
@@ -136,7 +146,8 @@ def transform_data_to_ngrams(file_name):
     for i in lines:
         if len(i) > 1:
             sentences[j].append(i.split()[0])
-            labels.append(i.split()[1].split("\n")[0])
+            if not test:
+                labels.append(i.split()[1].split("\n")[0])
         else:
             j += 1
             sentences.append([])
@@ -161,24 +172,30 @@ def transform_data_to_ngrams(file_name):
         run_on_sentence.extend(sentence)
 
     # put back with label
-    zipped_features_and_labels = list(zip(run_on_sentence, labels))
-    return zipped_features_and_labels, vocab_with_special_characters, pos_vocab
+    if not test:
+        zipped_features_and_labels = list(zip(run_on_sentence, labels))
+        return zipped_features_and_labels, vocab_with_special_characters, pos_vocab
+    else:
+        return run_on_sentence, vocab_with_special_characters, empty_spaces
 
 
 def tagger(task, train_file, valid_file, test_file):
     # prepare data
     five_grams, vocab, pos_tags = transform_data_to_ngrams(train_file)
     five_grams_valid, vocab_valid, pos_tags_valid = transform_data_to_ngrams(valid_file)
+    five_grams_test, vocab_test, empty_lines = transform_data_to_ngrams(test_file, True)
 
     # make word to index dictionary
     word_to_ix = {word: i for i, word in enumerate(vocab, start=1)}
 
     # make label to dictionary loss
     label_to_ix = {label: i for i, label in enumerate(pos_tags)}
+    ix_to_label = {i: label for i, label in enumerate(pos_tags)}
 
     # load data
     train_loader = DataLoader(five_grams, batch_size=BATCH_SIZE, shuffle=True)
-    valid_loader = DataLoader(five_grams_valid, batch_size=BATCH_SIZE, shuffle=True)
+    valid_loader = DataLoader(five_grams_valid, batch_size=BATCH_SIZE, shuffle=False)
+    test_loader = DataLoader(five_grams_test, batch_size=1, shuffle=False)
 
 
     # train
@@ -186,22 +203,46 @@ def tagger(task, train_file, valid_file, test_file):
     accuracies = []
     losses_valid = []
     accuracies_valid = []
-    lr = 0.5
+    lr = 0.8
     loss_function = nn.CrossEntropyLoss()
     model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE, len(label_to_ix.keys()))
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
-    for epoch in range(5):
+    for epoch in range(1):
         print("epoch is: {}".format(epoch))
         loss_t, accuracy_t = train(model, optimizer, loss_function, vocab, train_loader, word_to_ix, label_to_ix,
                                    len(five_grams))
         losses.append(loss_t)
         accuracies.append(accuracy_t)
-        loss_v, accuracy_v = validate(model, loss_function, valid_loader, word_to_ix, label_to_ix, len(five_grams_valid))
+        loss_v, accuracy_v, output = validate(model, loss_function, valid_loader, word_to_ix, label_to_ix,
+                                      len(five_grams_valid))
         losses_valid.append(loss_v)
         accuracies_valid.append(accuracy_v)
 
+    # with open("pos_results2.txt", "w") as f:
+    #     print("len of output is: ", len(output), len(five_grams_valid))
+    #     for elem in output:
+    #         # print("elem is: ", elem)
+    #         f.write(str(ix_to_label[elem[0]]) + "\n")
+    predictions = predict(model, loss_function, test_loader, word_to_ix, label_to_ix, len(five_grams_valid))
+    if task == "POS":
+        results_file_name = "pos_results1.txt"
+    else:
+        results_file_name = "ner_results.txt"
+    with open(results_file_name, "w") as f:
+        cnt = 0
+        print("pred length is: ", len(predictions) , " test length is: ", len(five_grams_test))
+        j = 0
+        for i in range(len(predictions) + len(empty_lines)):
+            if cnt in empty_lines: # to save format
+                f.write("\n")
+            else:
+                f.write(str(ix_to_label[predictions[j][0]]) + "\n")
+                j += 1
+            cnt += 1
+
     print("accuracies are:", accuracies, "validation accuracies: ", accuracies_valid)
+    print("loss are: ", losses, "valid losses", losses_valid)
 
     plt.plot(losses, label="train")
     plt.plot(losses_valid, label="validation")
@@ -221,8 +262,8 @@ def tagger(task, train_file, valid_file, test_file):
 
 
 if __name__ == "__main__":
-    tagger("POS tagger", train_file="pos/train", valid_file="pos/dev", test_file="pos/test")
-    tagger("NER tagger", train_file="ner/train", valid_file="ner/dev", test_file="ner/test")
+    tagger(task="POS", train_file="pos/train", valid_file="pos/dev", test_file="fake_test")
+    # tagger(task="NER", train_file="ner/train", valid_file="ner/dev", test_file="ner/test")
 
 """
 To do:
